4137a5267e10344dbe6b0a1dd1fcec9689aba4f2
Sapir Benzino <63209732+Sapir52@users.noreply.github.com>
2022-05-11 02:06:56 +0300

    Update Main.java

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,7 +1,5 @@
 package gui;
-
 import javax.swing.*;
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
@@ -9,37 +7,34 @@ import java.awt.event.ActionListener;
 
 
 public class Main extends JFrame implements ActionListener {
-   private static final long serialVersionUID = 1L;
-   private RoadSystemPanel panel;
-   private String[] names = {"Exit","Blue","None","Blue","Magenta","Orange", "Random","Help"};
-   private JMenu m1, m2, m3, m4, m5;
-   private JMenuItem[] mi;
-   private JMenuItem[] miDp;
-   private JMenuBar mb;
-   private JMenuItem m6,  m7;
-
-
-   
-   private String[] namesDp = {"CityBuilder","CountryBuilder"};
+	private static final long serialVersionUID = 1L;
+	private RoadSystemPanel panel;
+	private String[] names = {"Exit","Blue","None","Blue","Magenta","Orange", "Random","Help"};
+	private JMenu m1, m2, m3, m4, m5;
+	private JMenuItem[] mi;
+	private JMenuItem[] miDp;
+	private JMenuBar mb;
+	private JMenuItem m6,  m7;
+	private String[] namesDp = {"CityBuilder","CountryBuilder"};
 	/**
 	 * 
 	 * @param args
 	 */
-   public static void main(String[]args) {
-	   Main fr = new Main();
-	   fr.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-	   fr.setSize(845,715);
-	   fr.setVisible(true);
-   }
+	public static void main(String[]args) {
+		Main fr = new Main();
+		fr.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		fr.setSize(845,715);
+		fr.setVisible(true);
+	}
 
- /**
-  * default constructor
-  */
-   public Main() {
-	    super("Road system");
-	    panel = new RoadSystemPanel(this);
-	    add(panel);
-	    panel.setVisible(true);
+	/**
+	  * default constructor
+	*/
+	public Main() {
+	    	super("Road system");
+	    	panel = new RoadSystemPanel(this);
+	    	add(panel);
+	    	panel.setVisible(true);
 
 		mb = new JMenuBar();
 		m1 = new JMenu("File");
@@ -49,8 +44,8 @@ public class Main extends JFrame implements ActionListener {
 		mi = new JMenuItem[names.length];
 
 		for(int i=0;i<names.length;i++) {
-		    mi[i]=new JMenuItem(names[i]);
-		    mi[i].addActionListener(this);
+		    	mi[i]=new JMenuItem(names[i]);
+		    	mi[i].addActionListener(this);
 		}
 
 		
@@ -62,12 +57,9 @@ public class Main extends JFrame implements ActionListener {
 		}	
 		
 		m1.add(mi[0]);
-		
 		m2.add(mi[1]);
 		m2.addSeparator();
 		m2.add(mi[2]);
-
-
 		m3.add(mi[3]);
 		m3.addSeparator();
 		m3.add(mi[4]);
@@ -75,29 +67,21 @@ public class Main extends JFrame implements ActionListener {
 		m3.add(mi[5]);
 		m3.addSeparator();
 		m3.add(mi[6]);
-		
 		m4.add(mi[7]);
-
-		
 		m5.add(miDp[0]);
 		m5.addSeparator();		
 		m5.add(miDp[1]);
-
-
 		m6= new JMenuItem("Clone a car");
-
 		m7= new JMenuItem("Reports");
-
 		mb.add(m1);
 		mb.add(m2);
 		mb.add(m3);
 		mb.add(m4);
-		
 		mb.add(m5);
 		mb.add(m6);
 		mb.add(m7);
 		setJMenuBar(mb);
-   }
+	}
    
 	/**
 	 * 
